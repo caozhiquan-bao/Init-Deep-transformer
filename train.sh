230e6659d324a5f625025539622a90e1b1351506
caozhiquan-bao <59723461+caozhiquan-bao@users.noreply.github.com>
2021-11-27 19:36:08 +0800

    Add files via upload

--- train.sh
+++ train.sh
@@ -1,59 +1,33 @@
 #! /usr/bin/bash
 set -e
-
-#device=0,1,2,3,4,5,6,7
 device=0
 
-#task=iwslt-de2en
-task=wmt-en2de
+task=iwslt-de2en
+tag=001
 
-# must set this tag
-#tag=base-reg6-2_qkv_mean
-tag=base20-reg6_qk+1
-if [ $task == "wmt-en2de" ]; then
-        arch=fixup_heterogeneous_dense_transformer
-        share_embedding=0
-        share_decoder_input_output_embed=0
-        criterion=cross_entropy
-        fp16=0
-        lr=0.002
-        warmup=16000
-        max_tokens=8192
-        update_freq=4
-        weight_decay=0.0
-        keep_last_epochs=10
-        max_epoch=21
-        max_update=
-        data_dir=wmt16_en_de
-        src_lang=en
-        tgt_lang=de
-elif [ $task == "ldc" ]; then
-        arch=transformer_t2t_wmt_en_de
-        share_embedding=0
-        share_decoder_input_output_embed=1
-        fp16=1
-        lr=0.002
-        warmup=8000
-        max_tokens=2048
-        update_freq=4
-        weight_decay=0.0
-        keep_last_epochs=10
-        max_epoch=16
-        max_update=
-        data_dir=wmt16_en_de
-        src_lang=zh
-        tgt_lang=en
-else
-        echo "unknown task=$task"
-        exit
-fi
+arch=fixup_dense_relative_transformer_t2t_iwslt_de_en
+share_embedding=1
+share_decoder_input_output_embed=0
+criterion=label_smoothed_cross_entropy
+fp16=1
+lr=0.0015
+warmup=8000
+max_tokens=2048
+update_freq=2
+weight_decay=0.0001
+keep_last_epochs=30
+max_epoch=60
+data_dir=iwslt14.tokenized.de-en
+src_lang=de
+tgt_lang=en
+
 
 save_dir=checkpoints/$task/$tag
 
 if [ ! -d $save_dir ]; then
         mkdir -p $save_dir
 fi
-cp ${BASH_SOURCE[0]} $save_dir/train.sh
+
 
 gpu_num=`echo "$device" | awk '{split($0,arr,",");print length(arr)}'`
 
@@ -64,7 +38,7 @@ cmd="python3 -u train.py data-bin/$data_dir
   --lr-scheduler inverse_sqrt --warmup-init-lr 1e-07 --warmup-updates $warmup
   --lr $lr --min-lr 1e-09
   --weight-decay $weight_decay
-  --criterion $criterion
+  --criterion $criterion --label-smoothing 0.1
   --max-tokens $max_tokens
   --update-freq $update_freq
   --no-progress-bar
@@ -73,40 +47,11 @@ cmd="python3 -u train.py data-bin/$data_dir
   --save-dir $save_dir
   --keep-last-epochs $keep_last_epochs
   --tensorboard-logdir $save_dir
-  --decoder-layers 25
-  --encoder-layers 25
-  --no-scale-embedding
-  --Tfixup
-  --dont-use-layernorm
-  --encoder-normalize-before
-  --decoder-normalize-before"
-
-adam_betas="'(0.9, 0.997)'"
-cmd=${cmd}" --adam-betas "${adam_betas}
-if [ $share_embedding -eq 1 ]; then
-cmd=${cmd}" --share-all-embeddings "
-fi
-if [ $share_decoder_input_output_embed -eq 1 ]; then
-cmd=${cmd}" --share-decoder-input-output-embed "
-fi
-if [ -n "$max_epoch" ]; then
-cmd=${cmd}" --max-epoch "${max_epoch}
-fi
-if [ -n "$max_update" ]; then
-cmd=${cmd}" --max-update "${max_update}
-fi
-if [ -n "$dropout" ]; then
-cmd=${cmd}" --dropout "${dropout}
-fi
-if [ $fp16 -eq 1 ]; then
-cmd=${cmd}" --fp16 "
-fi
-
-#echo $cmd
-#eval $cmd
-#cmd=$(eval $cmd)
-#nohup $cmd exec 1> $save_dir/train.log exec 2>&1 &
-#tail -f $save_dir/train.log
+  --encoder-layer 50
+  --decoder-layer 6
+  --adam-betas '(0.9, 0.98)'
+  --share-all-embeddings
+  --fp16"
 
 export CUDA_VISIBLE_DEVICES=$device
 cmd="nohup "${cmd}" > $save_dir/train.log 2>&1 &"
